df9a2ee828b15e3f43c00a9f512e3e5b0a276a28
manuel-coder 02 <sotundeemmanuel@gmail.com>
2024-07-31 19:38:08 +0100

    latest commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const productList = document.querySelector('.product-list');
 const btnAddToCart = document.querySelector('.add-to-cart');
 const selectedCart = document.querySelector('.selected');
 const unSelectedCart = document.querySelector('.un-selected');
+const cartImage = document.querySelector('.product-image');
 const localJSON = async function () {
 	const res = await fetch('data.json');
 	const data = await res.json();
@@ -29,13 +30,24 @@ const localJSON = async function () {
 		// const realPrice = price.padEnd(2, 0);
 		// console.log(realPrice);
 		const html = `
-		<div class="products product-one">
+		<div class="products product-${i + 1}">
 			<img src="${
 				mov.image.mobile
 			}" alt="${mov.name.toLowerCase()}" class="product-image" />
 			
-			<button class="add-to-cart"> <img src="./assets/images/icon-add-to-cart.svg" alt="empty cart" /> Add to cart 
-			
+			<button class="add-to-cart">
+				<span class="un-selected">
+					<img src="./assets/images/icon-add-to-cart.svg" alt="empty cart" />
+					<p>Add to cart</p>
+				</span>
+
+				<span class="selected hidden">
+					<img src="./assets/images/icon-decrement-quantity.svg" alt="the dash symbol" class="symbol decrement" />
+
+				<span class="quantity">1</span>
+					<img src="./assets/images/icon-increment-quantity.svg" alt="the addition symbol"
+					class="symbol increment" />
+				</span>
 			</button>
 			
 			<div class="product-details">
@@ -58,4 +70,5 @@ btnAddToCart.addEventListener('click', function () {
 	btnAddToCart.style.border = 'none';
 	unSelectedCart.classList.add('hidden');
 	selectedCart.classList.remove('hidden');
+	cartImage.style.border = '2px solid hsl(14, 86%, 42%)';
 });
